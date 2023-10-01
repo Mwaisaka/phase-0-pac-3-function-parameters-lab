373cf673793fb7947ffa5a642e181bd6966d971c
Frank <frankkashere@gmail.com>
2023-10-01 11:41:06 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+function introduction(firstName) {
+    return `Hi, my name is ${firstName}.`
+  }
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+function introductionWithLanguageOptional(name, language="JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
